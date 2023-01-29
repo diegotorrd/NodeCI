d0ec5de7afb3010d0ef91da81c1b8df3bea8b718
Diego <diego40615@gmail.com>
2023-01-28 19:53:32 -0500

    Travis config

---
+++ __tests__/helpers/page.js
@@ -0,0 +1,49 @@
+const puppeteer = require("puppeteer");
+const sessionFactory = require("../factories/sessionFactory");
+const userFactory = require("../factories/userFactory");
+
+class CustomPage {
+    static async build() {
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox']
+        })
+
+        const page = await browser.newPage()
+        const customPage = new CustomPage(page)
+
+        return new Proxy(customPage, {
+            get: function(target, property) {
+                return customPage[property] || browser[property] || page[property];
+            }
+        })
+    }
+
+    constructor(page) {
+        this.page = page;
+    }
+
+    async login() {
+        // Create user in db
+        const user = await userFactory();
+
+        // Create session for user
+        const { session, sig } = sessionFactory(user);
+
+        // Login
+        await this.page.setCookie({ name: "session", value: session });
+        await this.page.setCookie({ name: "session.sig", value: sig });
+
+        await this.page.goto('http://localhost:3000/blogs')
+        await this.page.waitFor('a[href="/auth/logout"]'); //porque todo carga tan rapido que no llega a renderizar el logout
+
+    }
+
+    async getContentsOf(selector) {
+        return await this.page.$eval(selector, el => el.innerHTML)
+    }
+
+
+}
+
+module.exports = CustomPage
--- config/ci.js
+++ config/ci.js
@@ -1,7 +1,7 @@
 module.exports = {
     googleClientID: '502507394842-qlhkv9slu83c4vpm7dit9rdesqofi70c.apps.googleusercontent.com',
     googleClientSecret: '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',
-    mongoURI: 'mongodb+srv://generalUser:Wasabi95@cluster0.pc6ee.mongodb.net/blog_everyone',
+    mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
     cookieKey: '123123123',
     redisUrl: 'redis://127.0.0.1:6379'
 };
